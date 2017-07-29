63cd21049569f975120270af94c7c69db903a4e9
rex64 <rex64@users.noreply.github.com>
2017-07-29 12:53:45 +0200

    Basic player movements

---
+++ project/scripts/player.gd
@@ -0,0 +1,38 @@
+extends KinematicBody2D
+
+
+var speed = Vector2()
+var velocity = Vector2()
+ 
+const WALK_SPEED = 50
+const JUMP = 70
+const GRAVITY = 200
+
+func _ready():
+	set_process(true)
+	set_fixed_process(true)
+
+func _process(delta):
+	pass
+
+func _fixed_process(delta):
+
+	if Input.is_action_pressed("jump"):
+		velocity.y = -JUMP
+
+	velocity.y += delta * GRAVITY
+	if (Input.is_action_pressed("ui_left")):
+		velocity.x = - WALK_SPEED
+	elif (Input.is_action_pressed("ui_right")):
+		velocity.x =   WALK_SPEED
+	else:
+		velocity.x = 0
+
+	var motion = velocity * delta
+	motion = move(motion)
+
+	if (is_colliding()):
+		var n = get_collision_normal()
+		motion = n.slide(motion)
+		velocity = n.slide(velocity)
+		move(motion)
