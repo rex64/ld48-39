bb1b23aace1c636c4b0b7f01308e6c9d9fe4ecac
rex64 <rex64@users.noreply.github.com>
2017-07-30 12:49:48 +0200

    Hello Unity!

---
+++ assets/Player.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float translation = Input.GetAxis("Horizontal") * 1.0f;
+        transform.Translate(translation, 0.0f, 0.0f);
+    }
+}
